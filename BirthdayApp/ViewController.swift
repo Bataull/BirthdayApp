cff2950983856f6c8ff3c7f4411823d094b3c24d
Bataull <bataullinanton@gmail.com>
2024-02-03 10:05:58 +0300

    Initial commit

---
+++ BirthdayApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BirthdayApp
+//
+//  Created by Антон Батауллин on 3.02.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
